51d9308c58b78690f96b4fbfb77d0464db623c93
Dan Tsosie <dan@agilelaw.com>
2024-08-25 11:47:43 -0500

    Add opacity and enabled state

--- src/LevelObject.js
+++ src/LevelObject.js
@@ -2,17 +2,37 @@ import defaultLevelImage from './hobo_a.png';
 import React from 'react';
 
 class LevelObject extends React.Component {
+    state = {enabled: false};
+
     constructor(props) {
         super(props);
         this.props = props;
         return;
     }
 
+    handleOnMouseOver(e) {
+        if(e.type === 'mouseenter') {
+            e.target.style.opacity = 1;
+        }
+        else {
+            e.target.style.opacity = this.state.enabled ? 1 : .6;
+        }
+    }
+
     render() {
+        const style = {
+            width: this.props.imgScale,
+            left: this.props.xPos,
+            top: this.props.yPos
+        };
+        
+        style.opacity = this.state.enabled ? 1 : .6;
+        
         return (
             <div className='LevelObject'>
-                <img src={defaultLevelImage} alt='Default Object Representation' 
-                style={{ width: this.props.imgScale, left: this.props.xPos, top: this.props.yPos }}/>
+                <img src={defaultLevelImage} alt='Default Object Representation' style={style}
+                    onMouseEnter={this.handleOnMouseOver}
+                    onMouseLeave={this.handleOnMouseOver.bind(this)} />
             </div>
         )
     }
