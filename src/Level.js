113e03b4a7b98e7ee3521907948a345860982448
Dan Tsosie <dan@agilelaw.com>
2024-08-25 09:20:22 -0500

    Add position to level object

--- src/Level.js
+++ src/Level.js
@@ -1,10 +1,12 @@
 import levelImage from './level.jpg';
 import React from 'react';
+import LevelObject from './LevelObject';
 
 const Level = () => {
     return (
         <div className='Level'>
             <img src={levelImage} alt='Level Background' />
+            <LevelObject imgScale='18%' xPos='250px' yPos='350px' />
         </div>
     )
 };
--- src/LevelObject.js
+++ src/LevelObject.js
@@ -1,12 +1,21 @@
-import defaultLevelImage from './hobo_a.jpg';
+import defaultLevelImage from './hobo_a.png';
 import React from 'react';
 
-const LevelObject = () => {
-    return (
-        <div className='LevelObject'>
-            <img src={defaultLevelImage} alt='Default Object Representation' />
-        </div>
-    )
+class LevelObject extends React.Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+        return;
+    }
+
+    render() {
+        return (
+            <div className='LevelObject'>
+                <img src={defaultLevelImage} alt='Default Object Representation' 
+                style={{ width: this.props.imgScale, left: this.props.xPos, top: this.props.yPos }}/>
+            </div>
+        )
+    }
 };
 
 export default LevelObject;
